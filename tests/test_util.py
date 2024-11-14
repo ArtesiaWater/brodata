2a46e926714d055a2779580a5a823b0a02f945c1
Davíd Brakenhoff <d.brakenhoff@artesia-water.nl>
2024-11-14 11:23:28 +0100

    ruff

--- tests/test_util.py
+++ tests/test_util.py
@@ -3,5 +3,7 @@ import brodata
 
 
 def test_read_zipfile():
-    fname = os.path.join("tests", "data", "r-calje@artesia-water-nl_2024-06-04-12-35-07.zip")
+    fname = os.path.join(
+        "tests", "data", "r-calje@artesia-water-nl_2024-06-04-12-35-07.zip"
+    )
     brodata.util.read_zipfile(fname)
