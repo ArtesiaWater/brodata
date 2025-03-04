ad3ccfdfee257a1d2d431029c2bef734257e6707
Davíd Brakenhoff <d.brakenhoff@artesia-water.nl>
2025-03-04 10:38:32 +0100

    ruff

--- docs/conf.py
+++ docs/conf.py
@@ -6,22 +6,22 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'brodata'
-copyright = '2024, Artesia'
-author = 'Artesia'
-release = '0.0.0'
+project = "brodata"
+copyright = "2024, Artesia"
+author = "Artesia"
+release = "0.0.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'nbsphinx']
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "nbsphinx"]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
 html_theme_options = {"navigation_depth": 4}
