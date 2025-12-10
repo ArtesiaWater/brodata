192920543438c8fd14bc9a1bca69e9c484a493eb
Ruben Caljé <r.calje@artesia-water.nl>
2025-12-10 13:53:54 +0100

    Make pandas DataFrames scrollable

--- docs/conf.py
+++ docs/conf.py
@@ -26,6 +26,9 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
+html_css_files = [
+    'custom.css',
+]
 html_theme_options = {"navigation_depth": 4}
 
 nb_execution_mode = "auto"
